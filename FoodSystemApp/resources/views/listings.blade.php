28b01aac89b6999bbe4e73dddae31466833e6113
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 17:53:34 +0200

    referencing the components

--- FoodSystemApp/resources/views/listings.blade.php
+++ FoodSystemApp/resources/views/listings.blade.php
@@ -3,7 +3,7 @@
 @section('content')
             @unless(count($listings)==0)
             @foreach ($listings as $row)
-              
+              <x-listCard :row="$row"/>
             @endforeach
             @else
             <p>No listings data</p>
