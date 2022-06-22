301d7abf3262e99e901c6545dba5fdbd30551bc3
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 23:16:07 +0200

    removed the uncessary spaces

--- FoodSystemApp/resources/views/components/tag.blade.php
+++ FoodSystemApp/resources/views/components/tag.blade.php
@@ -6,12 +6,8 @@
  @endphp
  <ul class="flex">
  @foreach ($tag as $tageach)
-     
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="/?tag={{$tageach}}">{{$tageach}}</a>
-                                </li>
+     <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+        <a href="/?tag={{$tageach}}">{{$tageach}}</a>
+    </li>
  @endforeach
-
-                            </ul>
+</ul>
