2b8977e979942475c25ffc78c58d8252ca05b287
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 17:00:13 +0200

    adding the sytles

--- FoodSystemApp/resources/views/layout.blade.php
+++ FoodSystemApp/resources/views/layout.blade.php
@@ -101,5 +101,16 @@
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             >
        @yield('content');
+  <footer
+            class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+        >
+            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+
+            <a
+                href="create.html"
+                class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
+                >Post Job</a
+            >
+        </footer>
     </body>
 </html>
--- FoodSystemApp/resources/views/listings.blade.php
+++ FoodSystemApp/resources/views/listings.blade.php
@@ -1,17 +1,50 @@
 @extends ('layout')
 
 @section('content')
-    <h1>Listing</h1>
-            <ul>
             @unless(count($listings)==0)
             @foreach ($listings as $row)
-            <li>{{$row['id']}}</li>
-            <li>{{$row['title']}}</li>
-            <li>{{$row['description']}}</li>
+              <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img
+                            class="hidden w-48 mr-6 md:block"
+                            src="images/acme.png"
+                            alt="No image"
+                        />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">{{$row->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">{{$row->company}}</div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">{{$row->tags}}</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> {{$row->location}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
             @else
             <p>No listings data</p>
             @endunless
-            </ul>
- 
 @endsection()
