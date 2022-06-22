f00868e59fc9ad5b40661d94fb435694bfa9d74d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 21:55:49 +0200

    checking and filtering

--- FoodSystemApp/app/Http/Controllers/ListingController.php
+++ FoodSystemApp/app/Http/Controllers/ListingController.php
@@ -12,7 +12,7 @@ class ListingController extends Controller
         //we are going to filter using the scope filter in the controller
         // dd(request('tag'));
         $data['title']='Listings Route';
-        $data['listings']= Listing::latest()->get();
+        $data['listings']= Listing::latest()->filter(request(['tag']))->get();
         return view('listings',$data);
     }
     public function displayListing(Listing $listing){
