dfa55a42f6b493c6b5b350fa2a8b0b11eef2676c
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 23:23:39 +0200

    removing the pages

--- FoodSystemApp/resources/views/listings.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-            @unless(count($listings)==0)
-            @foreach ($listings as $row)
-              <x-listCard :row="$row"/>
-            @endforeach
-            @else
-            <p>No listings data</p>
-            @endunless
-</x-layout>
