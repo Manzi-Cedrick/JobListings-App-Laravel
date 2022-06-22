e56db841ad7a3e96220900a66f2d7577f4804b79
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 23:23:12 +0200

    changing the listing controller views

--- FoodSystemApp/app/Http/Controllers/ListingController.php
+++ FoodSystemApp/app/Http/Controllers/ListingController.php
@@ -13,10 +13,13 @@ class ListingController extends Controller
         // dd(request('tag'));
         $data['title']='Listings Route';
         $data['listings']= Listing::latest()->filter(request(['tags','search']))->get();
-        return view('listings',$data);
+        return view('listings.listings',$data);
     }
     public function displayListing(Listing $listing){
         $data['listings']= $listing;
-        return view('edit',$data);
+        return view('listings.singlelist',$data);
+    }
+    public function create(){
+        return view('listings.createlistings');
     }
 }
