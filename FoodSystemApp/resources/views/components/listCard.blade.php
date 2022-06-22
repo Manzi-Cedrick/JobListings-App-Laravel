889a36c34c131ed17180e2770f2c0391687ba5ef
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-22 18:09:16 +0200

    removing the tag list

--- FoodSystemApp/resources/views/components/listCard.blade.php
+++ FoodSystemApp/resources/views/components/listCard.blade.php
@@ -1,5 +1,5 @@
 @props(['row'])
-<x-card>
+<x-card class="p-0">
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
@@ -11,28 +11,7 @@
                                 <a href="/listings/{{$row->id}}">{{$row->title}}</a>
                             </h3>
                             <div class="text-xl font-bold mb-4">{{$row->company}}</div>
-                            <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">{{$row->tags}}</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                                >
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
+                           
                             <div class="text-lg mt-4">
                                 <i class="fa-solid fa-location-dot"></i> {{$row->location}}>
                             </div>
