4beecc8b4268bee54d5f126dc06c0575452c515d
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-23 14:26:00 +0200

    the new route

--- FoodSystemApp/resources/views/components/layout.blade.php
+++ FoodSystemApp/resources/views/components/layout.blade.php
@@ -83,7 +83,7 @@
             <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
             <a
-                href="/createNew"
+                href="/create"
                 class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
                 >Post Job</a
             >
