5d301ec2e4fea1ed49af86d6f653d49fcb377a24
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-24 16:33:48 +0200

    user id referencing

--- FoodSystemApp/app/Http/Controllers/ListingController.php
+++ FoodSystemApp/app/Http/Controllers/ListingController.php
@@ -29,15 +29,18 @@ class ListingController extends Controller
             'company'=>'required',
             'location' => 'required',
             'email'=>'required',
+            'logo' => 'required',
             'website' => 'required',
             'description'=>'required',
             'tags' => 'required'
         ]);
         if($request->hasFile('logo')){
             $formFields['logo'] = $request->file('logo')->store('logos','public');
-            Listing::create($formFields);
         }
-        return redirect('/')->with('message','Job Listing successfully created');
+        $formFields['user_id'] = auth()->id();
+        if(Listing::create($formFields)){
+            return redirect('/')->with('message','Job Listing successfully created');
+        };
     }
     public function edit(Listing $listing){
         return view('listings.editForm',['listings'=>$listing]);
