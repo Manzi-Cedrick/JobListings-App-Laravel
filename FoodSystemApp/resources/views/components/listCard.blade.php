11b43cafde191b613e91cb4c371ae8b840476156
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-24 10:38:03 +0200

    adding the logo row

--- FoodSystemApp/resources/views/components/listCard.blade.php
+++ FoodSystemApp/resources/views/components/listCard.blade.php
@@ -3,7 +3,7 @@
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
-                            src="images/acme.png"
+                            src="{{ $row->logo ? asset(' storage/ '. $row->logo) : asset('/images/no-image.png')}}"
                             alt="No image"
                         />
                         <div>
