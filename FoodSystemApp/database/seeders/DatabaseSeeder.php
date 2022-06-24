d292b94b54dafc26f1f12b344bf1a632fa272fb8
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-06-24 16:35:05 +0200

    seeding own data

--- FoodSystemApp/database/seeders/DatabaseSeeder.php
+++ FoodSystemApp/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Listing;
+use App\Models\User;
+// use Illuminate\Foundation\Auth\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,7 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Listing::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Mico Sydney',
+            'email' => 'john@gmail.com',
+            'password' => 'manzi1000'
+        ]);
+        Listing::factory(5)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
